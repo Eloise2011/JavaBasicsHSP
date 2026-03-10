e4e2f7ac16e2f9040e3095897a5890bf6578fdbc
Eloise2011 <joshuahbrooks@163.com>
2026-03-10 16:17:50 +0800

    代码块详解 - 有继承关系的子类对象创建过程

--- src/chapter10/src/com/hspedu/codeblock/CodeBlockDetail04.java
+++ src/chapter10/src/com/hspedu/codeblock/CodeBlockDetail04.java
@@ -21,6 +21,23 @@ public class CodeBlockDetail04 {
          * 7. Father 构造器
          * 8. Son 普通代码块
          * 9. Son 构造器
+         *
+         * 在子孙三代都加上普通方法和代码块后再次执行：
+         * 1. GrandFather static block
+         * 2. getA 静态方法
+         * 3. getB 静态方法
+         * 4. Father static block
+         * 5. Son static block
+         * 6. getC 静态方法
+         * 7. GrandFather 普通代码块
+         * 8. getA2 普通方法
+         * 9. GrandFather constructor
+         * 10. Father普通代码块
+         * 11. getB2 普通方法
+         * 12. Father 构造器
+         * 13. Son 普通代码块
+         * 14. getC2 普通方法
+         * 15. Son 构造器
          */
         new Son();
     }
@@ -29,7 +46,7 @@ public class CodeBlockDetail04 {
 class Son extends Father {
     public Son() {
         //super()
-        //本类代码块
+        //本类普通代码块和普通属性
         System.out.println("Son 构造器");
     }
 
@@ -40,12 +57,26 @@ class Son extends Father {
     static {
         System.out.println("Son static block");
     }
+
+    private static int c = getC();
+
+    public static int getC() {
+        System.out.println("getC 静态方法");
+        return 100;
+    }
+
+    public int c2 = getC2();
+
+    public int getC2() {
+        System.out.println("getC2 普通方法");
+        return 102;
+    }
 }
 
 class Father extends GrandFather {
     public Father() {
         //super()
-        //本类普通代码块
+        //本类普通代码块和普通属性
         System.out.println("Father 构造器");
     }
 
@@ -53,15 +84,29 @@ class Father extends GrandFather {
         System.out.println("Father普通代码块");
     }
 
+    private static int b = getB();
+
     static {
         System.out.println("Father static block");
+    }
+
+    public static int getB() {
+        System.out.println("getB 静态方法");
+        return 10;
+    }
+
+    public int b2 = getB2();
+
+    public int getB2() {
+        System.out.println("getB2 普通方法");
+        return 12;
     }
 }
 
 class GrandFather {
     public GrandFather() {
         //super()
-        //本类普通代码块
+        //本类普通代码块和普通属性
         System.out.println("GrandFather constructor");
     }
 
@@ -71,5 +116,19 @@ class GrandFather {
 
     static {
         System.out.println("GrandFather static block");
+    }
+
+    private static int a = getA();
+
+    public static int getA() {
+        System.out.println("getA 静态方法");
+        return 1;
+    }
+
+    public int a2 = getA2();
+
+    public int getA2() {
+        System.out.println("getA2 普通方法");
+        return 2;
     }
 }
